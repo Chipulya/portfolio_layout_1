09fcf5c1a430f01d36a5c73525afdd14b82757d2
Chipulya <nikmig2001936@gmail.com>
2022-08-22 19:13:02 +0300

    modal windows added

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ $(function() {
 
         $('html, body').animate({
             scrollTop: blockOffset - 50
-        }, 700)
+        }, 700);
     });
-});
+    
+        $('[data_modal]').on('click' , function(event){
+            event.preventDefault();
+            
+            $(modal_resume).addClass("show");
+    });
+
+    $(modal_resume).on('click', function() {
+        $(modal_resume).removeClass("show");
+    })
+}); 
+
